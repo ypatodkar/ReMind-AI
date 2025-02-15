a2fe061e11878adf831ba5227610c2782fd23ace
Saksham Yadav <sakshamyadav@Sakshams-MacBook-Air-2.local>
2025-02-15 12:14:31 -0800

    improved folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
