dca26040a642bd70f1164c89758e42cefda4445c
Yash Patodkar <yashpatodkar@Yashs-MacBook-Pro-8.local>
2025-02-15 12:47:09 -0800

    Conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,7 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import React from 'react';
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-      <h1 className=' '>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-=======
 const App = () => {
   return (
     <div>
-      
     </div>
   );
->>>>>>> a2fe061e11878adf831ba5227610c2782fd23ace
 }
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import React from 'react';
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
->>>>>>> a2fe061e11878adf831ba5227610c2782fd23ace
 
 const root = document.getElementById("root");
 
